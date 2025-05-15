1b4455314de2f640c79bac177c7d9b4a551d2b1c
Isaac Milarsky <imilarsky@gmail.com>
2025-05-15 13:52:55 -0500

    edit file save structure
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- scripts/pull-public-assets.py
+++ scripts/pull-public-assets.py
@@ -24,27 +24,21 @@ def main():
                 token = github_key
             )
 
-            agency_dir = os.path.join(AGENCY_CODEJSON_DIR, agency)
 
             try:
                 os.mkdir(AGENCY_CODEJSON_DIR)
             except FileExistsError:
                 print(f"File already exists: {AGENCY_CODEJSON_DIR}")
 
-            try:
-                os.mkdir(agency_dir)
-            except FileExistsError:
-                print(f"File already exists: {agency_dir}")
-
             orgs = codegovDict[agency]["orgs"]
             for org in orgs:
                 org = org.strip()
                 indexGen.process_organization(org)
 
-                indexGen.save_organization_files(org, agency_dir)
+                indexGen.save_organization_files(org, AGENCY_CODEJSON_DIR)
 
-            indexGen.save_index(os.path.join(agency_dir, agency + "_index.json"))
-            print(f"\nIndexing complete. Results saved to {agency_dir}")
+            indexGen.save_index(os.path.join(AGENCY_CODEJSON_DIR, agency + "_index.json"))
+            print(f"\nIndexing complete. Results saved to {AGENCY_CODEJSON_DIR}")
         
     except Exception as e:
         print(f"Error: {e}")
